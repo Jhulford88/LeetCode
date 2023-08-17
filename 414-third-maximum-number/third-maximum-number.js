b162b37e8af8d2b50bf40daf2dfe5d904756e22a
Joshua Edward Hulford <joshua.hulford@gmail.com>
2023-08-17 10:25:34 -0400

    Time: 66 ms (36.75%) | Memory: 44.3 MB (50.20%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.js
+++ 414-third-maximum-number/third-maximum-number.js
@@ -7,6 +7,6 @@ var thirdMax = function(nums) {
 
     let unique = Array.from(new Set(nums)).sort((a,b) => a - b);
     if (unique.length < 3) return Math.max(...unique)
-    console.log('unique.....',unique)
+
     return unique[unique.length-3]
 };
