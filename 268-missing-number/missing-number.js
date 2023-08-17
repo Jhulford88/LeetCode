c2a02954f7d3bbac1340ec70112c6348cdb23f8e
Joshua Edward Hulford <joshua.hulford@gmail.com>
2023-08-16 20:35:02 -0400

    Time: 62 ms (69.86%) | Memory: 45 MB (21.16%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -6,8 +6,7 @@ var missingNumber = function(nums) {
     let n = nums.length;
     let range = []
     while (n > -1) {
-        range.push(n); 
-        n--;
+        range.push(n), n--;
         }
     
     return range.reduce((a,b) => a + b) - nums.reduce((a,b) => a + b)
