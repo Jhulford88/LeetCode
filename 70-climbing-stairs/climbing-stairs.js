51dc461e747e1a1d44264f5bbc8f7bd2b590c22a
Joshua Edward Hulford <joshua.hulford@gmail.com>
2023-08-22 13:32:32 -0400

    Time: 47 ms (86.19%) | Memory: 42.1 MB (13.47%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+ //
+var climbStairs = function(n) {
+    let numOfWays = [1,2];
+    for (let i = 2; i < n; i++) {
+        numOfWays[i] = numOfWays[i - 1] + numOfWays[i - 2];
+    }
+    return numOfWays[n-1];
+};
