9a4d2bd46c528e2a7cc122c5296e3aac417ec35b
Joshua Edward Hulford <joshua.hulford@gmail.com>
2023-08-22 13:45:09 -0400

    Time: 74 ms (51.26%) | Memory: 48.5 MB (43.11%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+    let n = s.length;
+    for(let i = 1; i <= n / 2; i++) {
+        if (n % i === 0) {
+            const substring = s.slice(0, i);
+            let repeated = "";
+            for (let j = 0; j < n / i; j++) {
+                repeated += substring;
+            }
+            if (repeated === s) {
+                return true;
+            }
+        }
+    }
+    return false
+};
+
+
+
+
+
+
+
