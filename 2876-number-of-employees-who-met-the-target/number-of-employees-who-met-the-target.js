cdfea927a26b3c997169987b3d1abb54e230cf53
Joshua Edward Hulford <joshua.hulford@gmail.com>
2023-08-24 12:56:44 -0400

    Time: 58 ms (53.71%) | Memory: 43.6 MB (61.27%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.js
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.js
@@ -4,11 +4,5 @@
  * @return {number}
  */
 var numberOfEmployeesWhoMetTarget = function(hours, target) {
-    let count = 0;
-    for (let i = 0; i < hours.length; i++) {
-        if (hours[i] >= target) {
-            count++;
-        }
-    }
-    return count;
+   return hours.filter(n => n >= target).length
 };
