84fd0c7c946b248957e265e3fef08ddea30979d7
Joshua Edward Hulford <joshua.hulford@gmail.com>
2023-09-08 14:15:28 -0400

    Time: 58 ms (35.74%) | Memory: 42 MB (52.96%) - LeetSync

---
+++ 1137-height-checker/height-checker.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    let count = 0;
+    let expected = heights.slice().sort((a,b) => a - b)
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== expected[i]) count++;
+    }
+    return count
+};
