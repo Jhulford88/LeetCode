0a32525cd424fa3fcc2b7eb5e21358edd2f9de84
Joshua Edward Hulford <joshua.hulford@gmail.com>
2023-09-09 14:27:08 -0400

    Time: 55 ms (62.34%) | Memory: 43.2 MB (63.35%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    while (stones.length > 1) {
+    stones.sort((a,b) => a - b);
+    if (stones[stones.length-1] - stones[stones.length-2] === 0) {
+        stones.pop();
+        stones.pop();
+    } else if (stones[stones.length-1] - stones[stones.length-2] != 0) {
+        stones[stones.length-2] = stones[stones.length-1] - stones[stones.length-2];
+        stones.pop()
+    }
+}
+    return stones;
+};
