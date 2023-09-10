df1ed6b483ea0f6cad8e52671d7026ec4f568a72
Joshua Edward Hulford <joshua.hulford@gmail.com>
2023-09-10 12:52:27 -0400

    Time: 59 ms (30.00%) | Memory: 42.1 MB (35.83%) - LeetSync

---
+++ 1115-valid-boomerang/valid-boomerang.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} points
+ * @return {boolean}
+ */
+var isBoomerang = function(points) {
+    // x1 (y2 - y3) + x2 (y3 - y1) + x3 (y1 - y2) = 0
+    let [[x1, y1], [x2, y2], [x3, y3]] = points
+    return x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) == 0 ? false : true
+};
