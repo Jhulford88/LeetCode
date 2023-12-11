86378c33b0b5dc476b1e58815a49194c522ace53
Joshua Edward Hulford <joshua.hulford@gmail.com>
2023-12-11 15:36:26 -0500

    Time: 66 ms (46.93%) | Memory: 44.4 MB (74.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,11 +3,17 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    for(let i=0;i<nums.length;){
-     if(nums[i] ===nums[i+1]){
-         nums.splice(i,1)
-     }else{
-         i++
-     }
- }
+   if (nums.length === 0) {
+        return 0;
+    }
+
+    let k = 1; // Initialize the count of unique elements to 1
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 1]) {
+            nums[k] = nums[i]; // Overwrite the next unique element
+            k++;
+        }
+    }
+
+    return k;
 };
